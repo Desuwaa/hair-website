5b6795ebdd8efde60a385294c879e27d23c0f004
Desuwaa <airekpita@gmail.com>
2023-08-24 12:11:08 +0100

    checkpoint

---
+++ todoapp/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/Reducer';
+import AddTask from './components/AddTasks';
+import TaskList from './components/ListTask';
+
+const store = createStore(rootReducer); // Create your Redux store
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div>
+        <h1>ToDo Application</h1>
+        <AddTask />
+        <TaskList />
+        <h2>Completed Tasks</h2>
+        <TaskList filter="done" />
+        <h2>Remaining Tasks</h2>
+        <TaskList filter="notDone" />
+      </div>
+    </Provider>
+  );
+};
+
+export default App;
